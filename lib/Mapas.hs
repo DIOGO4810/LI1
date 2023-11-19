db1a172d09455822b37944ac3836a43d7a4a9618
Heitor Fernandes <heitoraraujofernandes03@gmail.com>
2023-11-19 20:22:50 +0000

    Correção nos comentários em haddock; Matriz do mapa 1 adicionada

---
+++ lib/Mapas.hs
@@ -0,0 +1,43 @@
+{-|
+Module      : Mapas
+Description : Matrizes dos mapas
+Copyright   : Diogo José Ribeiro Ribeiro <a106906@alunos.uminho.pt>
+              Heitor Araújo Fernandes <a106933@alunos.uminho.pt>
+
+Módulo com as matrizes dos mapas e as posições inicial e final do jogador para cada nível.
+-}
+
+module Mapas where
+
+import LI12324
+
+
+-- | Matriz do mapa do nível 1
+
+mapa1 = Mapa ((1.5, 18.5), Este) (7.5, 2.5)
+[[Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio]
+,[Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio]
+,[Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio]
+,[Vazio,Vazio,Vazio,Vazio,Vazio,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Vazio,Vazio,Vazio,Vazio,Vazio]
+,[Vazio,Vazio,Vazio,Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Vazio,Vazio]
+,[Vazio,Vazio,Vazio,Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Vazio,Vazio]
+,[Vazio,Vazio,Vazio,Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Vazio,Vazio]
+,[Vazio,Plataforma,Plataforma,Vazio,Vazio,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Vazio,Vazio,Plataforma,Plataforma,Vazio]
+,[Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Escada,Vazio,Vazio]
+,[Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Escada,Vazio,Vazio]
+,[Vazio,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Alcapao,Alcapao,Alcapao,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Vazio]
+,[Vazio,Vazio,Vazio,Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Vazio,Vazio]
+,[Vazio,Vazio,Vazio,Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Vazio,Vazio]
+,[Vazio,Vazio,Plataforma,Plataforma,Plataforma,Plataforma,Alcapao,Alcapao,Alcapao,Plataforma,Plataforma,Plataforma,Plataforma,Vazio,Vazio]
+,[Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Escada,Vazio,Vazio]
+,[Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Escada,Vazio,Vazio]
+,[Vazio,Plataforma,Plataforma,Alcapao,Alcapao,Alcapao,Plataforma,Plataforma,Plataforma,Alcapao,Alcapao,Alcapao,Plataforma,Plataforma,Vazio]
+,[Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio]
+,[Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio]
+,[Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma]
+
+]
+
+
+
+
--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -10,23 +10,23 @@ module Tarefa1 where
 
 import LI12324
 
--- Função para verificar colisões com plataformas e os limites do Mapa laterais e superior
+-- | Função para verificar colisões com plataformas e os limites do Mapa laterais e superior
 colisoesParede :: Mapa -> Personagem -> Bool
 colisoesParede (Mapa (_, _) (limiteX, limiteY) matriz) personagem = colide
   where
     (px, py) = posicao personagem
     (tamanhoX, tamanhoY) = tamanho personagem
-    -- Verifica se o personagem está fora dos limites laterais
+    -- | Verifica se o personagem está fora dos limites laterais
     foraDosLimitesLaterais = px - tamanhoX / 2 < 0 || px + tamanhoX / 2 > limiteX
-    -- Verifica se o personagem está abaixo do limite superior (não colide com o topo)
+    -- | Verifica se o personagem está abaixo do limite superior (não colide com o topo)
     abaixoDoLimiteSuperior = py + tamanhoY / 2 < limiteY
-    -- Verifica se o personagem está em algum bloco de plataforma investigando os elementos verticais e horizontais da matriz
+    -- | Verifica se o personagem está em algum bloco de plataforma investigando os elementos verticais e horizontais da matriz
     emPlataforma i j = case matriz !! i !! j of
       Plataforma -> True
       _ -> False
-    -- Verifica se o personagem colide com a plataforma usando a função floor para combinar os valores reais recebidos com os inteiros da matriz
+    -- | Verifica se o personagem colide com a plataforma usando a função floor para combinar os valores reais recebidos com os inteiros da matriz
     colideComPlataforma l = any (\i -> any (\j -> emPlataforma i j) [floor px, floor (px + tamanhoX)]) l
-    -- Combinando as condições na função principal
+    -- | Combinando as condições na função principal
     colide = foraDosLimitesLaterais || (abaixoDoLimiteSuperior && (colideComPlataforma [floor px, floor (px + tamanhoX)]))
 
 
@@ -41,7 +41,7 @@ colisoesPersonagens p1 p2 = colide
     hitboxP2 = calculaHitbox p2
     colide = colisaoHitbox hitboxP1 hitboxP2
 
--- Função auxiliar para calcular a hitbox de um personagem considerando que px e py estão no respetivo centro
+-- | Função auxiliar para calcular a hitbox de um personagem considerando que px e py estão no respetivo centro
 calculaHitbox :: Personagem -> Hitbox
 calculaHitbox personagem =
   ((px - tamanhoX / 2, py - tamanhoY / 2), (px + tamanhoX / 2, py + tamanhoY / 2))
@@ -49,6 +49,6 @@ calculaHitbox personagem =
     (px, py) = posicao personagem
     (tamanhoX, tamanhoY) = tamanho personagem
 
--- Função auxiliar para verificar colisão entre duas hitboxes
+-- | Função auxiliar para verificar colisão entre duas hitboxes
 colisaoHitbox :: Hitbox -> Hitbox -> Bool
 colisaoHitbox ((x1, y1), (x2, y2)) ((x3, y3), (x4, y4)) = x1 < x4 && x2 > x3 && y1 < y4 && y2 > y3
